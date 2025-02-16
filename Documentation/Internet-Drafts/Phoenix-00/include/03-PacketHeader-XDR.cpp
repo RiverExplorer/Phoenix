8bf42ce7bdb4ec2fcba6c810b35295b17d0b2ca2
Doug Royer <DouglasRoyer@gmail.com>
2025-02-16 09:58:03 -0800

    Now the docs pull the XDR file from the sample implementation. So I can edit them in the code, and it get updated in the docs without having to sync the code.

---
+++ Documentation/Internet-Drafts/Phoenix-00/include/03-PacketHeader-XDR.cpp
@@ -0,0 +1,3 @@
+<artwork type="abnf" name="" align="left" alt=""><![CDATA[
+#include "../../../../libPhoenix/PacketHeader.x"
+]]></artwork>
---
+++ Documentation/Internet-Drafts/Phoenix-00/include/06-2-ServerConfigure-XDR.cpp
@@ -0,0 +1,3 @@
+<artwork type="abnf" name="" align="left" alt=""><![CDATA[
+#include "../../../../libPhoenix/ServerConfigReplyPayload.x"
+]]></artwork>
